abb906eec9f2f4f43c1685901911d9e4867a9cc9
qweowt <klimenkonikita81@gmail.com>
2024-05-15 01:00:07 +0300

    задачи 1-3

--- TechVisionLab4/Form1.Designer.cs
+++ TechVisionLab4/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace TechVisionLab4
+﻿
+namespace TechVisionLab4
 {
     partial class Form1
     {
@@ -35,7 +36,25 @@
             MultiButton = new Button();
             openFileDialog1 = new OpenFileDialog();
             updateFrame = new System.Windows.Forms.Timer(components);
+            filtersCb = new ComboBox();
+            BinParamBox = new GroupBox();
+            trackBarBlur = new TrackBar();
+            trackBarUR = new TrackBar();
+            trackBarUG = new TrackBar();
+            trackBarUB = new TrackBar();
+            trackBarLR = new TrackBar();
+            trackBarLG = new TrackBar();
+            trackBarLB = new TrackBar();
+            PixelParm = new CheckBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            BinParamBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarBlur).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarUR).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarUG).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarUB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarLR).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarLG).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarLB).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -46,6 +65,7 @@
             pictureBox1.Size = new Size(640, 480);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.MouseMove += ParamCheck;
             // 
             // RbImage
             // 
@@ -87,14 +107,119 @@
             // 
             // updateFrame
             // 
-            updateFrame.Interval = 50;
+            updateFrame.Interval = 34;
             updateFrame.Tick += updateFrame_Tick;
+            // 
+            // filtersCb
+            // 
+            filtersCb.FormattingEnabled = true;
+            filtersCb.Items.AddRange(new object[] { "orginal", "CvtColor", "InRangeS", "Canny", "Filter2D", "Smooth" });
+            filtersCb.Location = new Point(668, 531);
+            filtersCb.Name = "filtersCb";
+            filtersCb.Size = new Size(160, 23);
+            filtersCb.TabIndex = 4;
+            filtersCb.SelectedIndexChanged += filtersCb_SelectedIndexChanged;
+            // 
+            // BinParamBox
+            // 
+            BinParamBox.Controls.Add(trackBarBlur);
+            BinParamBox.Controls.Add(trackBarUR);
+            BinParamBox.Controls.Add(trackBarUG);
+            BinParamBox.Controls.Add(trackBarUB);
+            BinParamBox.Controls.Add(trackBarLR);
+            BinParamBox.Controls.Add(trackBarLG);
+            BinParamBox.Controls.Add(trackBarLB);
+            BinParamBox.Location = new Point(658, 298);
+            BinParamBox.Name = "BinParamBox";
+            BinParamBox.Size = new Size(335, 227);
+            BinParamBox.TabIndex = 5;
+            BinParamBox.TabStop = false;
+            BinParamBox.Text = "Filter Parametrs";
+            // 
+            // trackBarBlur
+            // 
+            trackBarBlur.Location = new Point(172, 175);
+            trackBarBlur.Maximum = 100;
+            trackBarBlur.Minimum = 1;
+            trackBarBlur.Name = "trackBarBlur";
+            trackBarBlur.Size = new Size(160, 45);
+            trackBarBlur.TabIndex = 6;
+            trackBarBlur.TickStyle = TickStyle.None;
+            trackBarBlur.Value = 1;
+            trackBarBlur.Visible = false;
+            // 
+            // trackBarUR
+            // 
+            trackBarUR.Location = new Point(172, 22);
+            trackBarUR.Maximum = 255;
+            trackBarUR.Name = "trackBarUR";
+            trackBarUR.Size = new Size(160, 45);
+            trackBarUR.TabIndex = 5;
+            trackBarUR.Visible = false;
+            // 
+            // trackBarUG
+            // 
+            trackBarUG.Location = new Point(172, 73);
+            trackBarUG.Maximum = 255;
+            trackBarUG.Name = "trackBarUG";
+            trackBarUG.Size = new Size(160, 45);
+            trackBarUG.TabIndex = 4;
+            trackBarUG.Visible = false;
+            // 
+            // trackBarUB
+            // 
+            trackBarUB.Location = new Point(172, 124);
+            trackBarUB.Maximum = 255;
+            trackBarUB.Name = "trackBarUB";
+            trackBarUB.Size = new Size(160, 45);
+            trackBarUB.TabIndex = 3;
+            trackBarUB.Visible = false;
+            // 
+            // trackBarLR
+            // 
+            trackBarLR.Location = new Point(6, 22);
+            trackBarLR.Maximum = 255;
+            trackBarLR.Name = "trackBarLR";
+            trackBarLR.Size = new Size(160, 45);
+            trackBarLR.TabIndex = 2;
+            trackBarLR.Visible = false;
+            // 
+            // trackBarLG
+            // 
+            trackBarLG.Location = new Point(6, 73);
+            trackBarLG.Maximum = 255;
+            trackBarLG.Name = "trackBarLG";
+            trackBarLG.Size = new Size(160, 45);
+            trackBarLG.TabIndex = 1;
+            trackBarLG.Visible = false;
+            // 
+            // trackBarLB
+            // 
+            trackBarLB.Location = new Point(6, 124);
+            trackBarLB.Maximum = 255;
+            trackBarLB.Name = "trackBarLB";
+            trackBarLB.Size = new Size(160, 45);
+            trackBarLB.TabIndex = 0;
+            trackBarLB.Visible = false;
+            // 
+            // PixelParm
+            // 
+            PixelParm.AutoSize = true;
+            PixelParm.Location = new Point(856, 535);
+            PixelParm.Name = "PixelParm";
+            PixelParm.Size = new Size(136, 19);
+            PixelParm.TabIndex = 6;
+            PixelParm.Text = "Read pixel parametrs";
+            PixelParm.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(887, 566);
+            ClientSize = new Size(1003, 563);
+            Controls.Add(PixelParm);
+            Controls.Add(BinParamBox);
+            Controls.Add(filtersCb);
             Controls.Add(MultiButton);
             Controls.Add(RbCam);
             Controls.Add(RbImage);
@@ -104,9 +229,20 @@
             FormClosing += MainForm_FormClosing;
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            BinParamBox.ResumeLayout(false);
+            BinParamBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarBlur).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarUR).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarUG).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarUB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarLR).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarLG).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarLB).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
+
+
 
         #endregion
 
@@ -116,5 +252,15 @@
         private Button MultiButton;
         private OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Timer updateFrame;
+        private ComboBox filtersCb;
+        private GroupBox BinParamBox;
+        private TrackBar trackBarUB;
+        private TrackBar trackBarLR;
+        private TrackBar trackBarLG;
+        private TrackBar trackBarLB;
+        private TrackBar trackBarUR;
+        private TrackBar trackBarUG;
+        private TrackBar trackBarBlur;
+        private CheckBox PixelParm;
     }
 }
--- TechVisionLab4/Form1.cs
+++ TechVisionLab4/Form1.cs
@@ -1,6 +1,8 @@
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
+using System.Drawing;
 using System.Windows.Forms;
+using static OpenCvSharp.Stitcher;
 
 namespace TechVisionLab4
 {
@@ -11,6 +13,9 @@ namespace TechVisionLab4
         Mat frame;
         VideoCapture videoCapture;
         bool camIsOn = false;
+
+        OpenCvSharp.Point point;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +24,13 @@ namespace TechVisionLab4
         private void RbImage_CheckedChanged(object sender, EventArgs e)
         {
             MultiButton.Text = "Select Image";
+            updateFrame.Stop();
         }
 
         private void RbCam_CheckedChanged(object sender, EventArgs e)
         {
             MultiButton.Text = "Enable Camera";
+            updateFrame.Stop();
         }
 
         private void MultiButton_Click(object sender, EventArgs e)
@@ -35,6 +42,10 @@ namespace TechVisionLab4
             }
             else if (RbImage.Checked)
             {
+                updateFrame.Stop();
+                videoCapture?.Dispose();
+                frame?.Dispose();
+                camIsOn = false;
                 SelectImage();
             }
             else
@@ -50,8 +61,8 @@ namespace TechVisionLab4
                 string a = openFileDialog1.FileName;
                 buffer = new Bitmap(a);
                 pictureBox1.Image = buffer;
+                updateFrame.Start();
             }
-
         }
 
         private void OnOffCam()
@@ -80,21 +91,32 @@ namespace TechVisionLab4
             frame?.Dispose();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-           
-        }
-
         private void updateFrame_Tick(object sender, EventArgs e)
         {
             try
             {
-                frame = new Mat();
-                videoCapture.Read(frame);
+                if (!camIsOn)
+                {
+                    frame = BitmapConverter.ToMat(buffer);
+                }
+                else
+                {
+                    frame = new Mat();
+                    videoCapture.Read(frame);
+                }
 
                 if (!frame.Empty())
                 {
-                    pictureBox1.Image = BitmapConverter.ToBitmap(frame); // Отображение кадра на PictureBox
+                    if(PixelParm.Checked)
+                    {
+                        Vec3b pixelValue = frame.Get<Vec3b>(point.Y, point.X);
+                        byte b = pixelValue.Item0;
+                        byte r = pixelValue.Item1;
+                        byte g = pixelValue.Item2;
+                        string str = $"B: {b.ToString()} R: {r.ToString()} G: {g.ToString()}";
+                        frame.PutText(str, point, new HersheyFonts(), 0.5, new Scalar(0, 0, 0));
+                    }
+                    pictureBox1.Image = BitmapConverter.ToBitmap(SetFilter(frame)); // Отображение кадра на PictureBox
                 }
             }
             catch (Exception ex)
@@ -102,6 +124,138 @@ namespace TechVisionLab4
                 MessageBox.Show(ex.Message);
                 updateFrame.Stop();
             }
+        }
+
+        private Mat SetFilter(Mat frame)
+        {
+            Mat newFrame = new Mat();
+            switch (filtersCb.SelectedIndex)
+            {
+                case 0:
+                    SetUIParam(filtersCb.SelectedIndex);
+                    newFrame = frame;
+                    break;
+                case 1:
+                    Cv2.CvtColor(frame, newFrame, ColorConversionCodes.RGB2GRAY);
+                    SetUIParam(filtersCb.SelectedIndex);
+                    break;
+                case 2:
+                    Scalar lowerBound = new Scalar(trackBarLR.Value, trackBarLG.Value, trackBarLG.Value); // Нижняя граница (синий цвет)
+                    Scalar upperBound = new Scalar(trackBarUR.Value, trackBarUG.Value, trackBarUB.Value); // Верхняя граница (зеленый цвет)
+                    Cv2.InRange(frame, lowerBound, upperBound, newFrame);
+                    SetUIParam(filtersCb.SelectedIndex);
+                    break;
+                case 3:
+                    SetUIParam(filtersCb.SelectedIndex);
+                    Cv2.Canny(frame, newFrame, trackBarLR.Value, trackBarUR.Value);
+                    break;
+                case 4:
+                    Mat filtered2D = new Mat(3, 3, MatType.CV_32F, new float[] { -1, -1, -1, -1, 8, -1, -1, -1, -1 }); // Создание матрицы ядра фильтра
+                    SetUIParam(filtersCb.SelectedIndex);
+                    Cv2.Filter2D(frame, newFrame, -1, filtered2D);
+                    break;
+                case 5:
+                    OpenCvSharp.Size kernelSize = new OpenCvSharp.Size(trackBarBlur.Value, trackBarBlur.Value); // Размер ядра сглаживания
+                    SetUIParam(filtersCb.SelectedIndex);
+                    Cv2.Blur(frame, newFrame, kernelSize);
+                    break;
+                default:
+                    SetUIParam(filtersCb.SelectedIndex);
+                    newFrame = frame;
+                    break;
+            }
+
+            return newFrame;
+        }
+        private void SetUIParam(int code)
+        {
+            switch (code)
+            {
+                case 0:
+                    trackBarBlur.Visible = false;
+                    trackBarLB.Visible = false;
+                    trackBarLG.Visible = false;
+                    trackBarLR.Visible = false;
+                    trackBarUB.Visible = false;
+                    trackBarUG.Visible = false;
+                    trackBarUR.Visible = false;
+                    break;
+                case 1:
+                    trackBarBlur.Visible = false;
+                    trackBarLB.Visible = false;
+                    trackBarLG.Visible = false;
+                    trackBarLR.Visible = false;
+                    trackBarUB.Visible = false;
+                    trackBarUG.Visible = false;
+                    trackBarUR.Visible = false;
+                    break;
+                case 2:
+                    trackBarBlur.Visible = false;
+                    trackBarLB.Visible = true;
+                    trackBarLG.Visible = true;
+                    trackBarLR.Visible = true;
+                    trackBarUB.Visible = true;
+                    trackBarUG.Visible = true;
+                    trackBarUR.Visible = true;
+                    break;
+                case 3:
+                    trackBarBlur.Visible = false;
+                    trackBarLB.Visible = false;
+                    trackBarLG.Visible = false;
+                    trackBarLR.Visible = true;
+                    trackBarUB.Visible = false;
+                    trackBarUG.Visible = false;
+                    trackBarUR.Visible = true;
+                    break;
+                case 4:
+                    trackBarBlur.Visible = false;
+                    trackBarLB.Visible = false;
+                    trackBarLG.Visible = false;
+                    trackBarLR.Visible = false;
+                    trackBarUB.Visible = false;
+                    trackBarUG.Visible = false;
+                    trackBarUR.Visible = false;
+                    break;
+                case 5:
+                    trackBarBlur.Visible = true;
+                    trackBarLB.Visible = false;
+                    trackBarLG.Visible = false;
+                    trackBarLR.Visible = false;
+                    trackBarUB.Visible = false;
+                    trackBarUG.Visible = false;
+                    trackBarUR.Visible = false;
+                    break;
+                default:
+                    trackBarBlur.Visible = false;
+                    trackBarLB.Visible = false;
+                    trackBarLG.Visible = false;
+                    trackBarLR.Visible = false;
+                    trackBarUB.Visible = false;
+                    trackBarUG.Visible = false;
+                    trackBarUR.Visible = false;
+                    break;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filtersCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetUIParam(filtersCb.SelectedIndex);
+        }
+
+        private void ParamCheck(object sender, MouseEventArgs e)
+        {
+            if (PixelParm.Checked)
+            {
+                point = new OpenCvSharp.Point(e.X, e.Y);
+            }
+            else
+                return;
         }
     }
 }
